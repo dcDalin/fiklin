49ca5567a1b0964d984c9c6a4da9f67ce4012616
Dalin Oluoch <mcdalinoluoch@gmail.com>
2020-11-26 16:58:32 +0300

    Setup deployment

--- actions/src/index.ts
+++ actions/src/index.ts
@@ -1,2 +1,40 @@
-// tslint:disable-next-line: no-console
-console.log('HELLO FROM TYPESCRIPT');
+import path from 'path';
+import express from 'express';
+
+const PORT = process.env.PORT || 4000;
+
+(async () => {
+  try {
+    const app = express();
+
+    const staticFiles = express.static(
+      path.join(__dirname, '../../client/build'),
+    );
+
+    app.use(staticFiles);
+
+    // tslint:disable-next-line: variable-name
+    app.get('/api/v1/test', (_req, res) => {
+      res.send('hello');
+    });
+
+    // all react routes
+    app.use('/*', staticFiles);
+
+    app.listen({ port: PORT }, () =>
+      // tslint:disable-next-line: no-console
+      console.log(
+        `
+        ################################################
+
+        🚀 Server ready at http://localhost:${PORT} 🚀
+
+        ################################################
+        `,
+      ),
+    );
+  } catch (e) {
+    // tslint:disable-next-line: no-console
+    console.error(e);
+  }
+})();
